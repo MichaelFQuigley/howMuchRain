b909c1b57c967cc857f6b52d34513b2cad26485b
Zach <me@zacharywilcox.net>
2015-03-25 23:01:51 -0600

    really cleaned up merger conflicts

--- project/main.py
+++ project/main.py
@@ -30,31 +30,20 @@ if __name__ == "__main__":
     trainHeaders = getHeaders(args.train)
 
     print 'finding max for test data'
-<<<<<<< HEAD
-    testHeaders =   getHeaders(args.test)
-    
-=======
-    testHeaders, testMax =  findMaxColumnDim(ColInd.TimeToEnd, args.test)
 
->>>>>>> c830757188f5de11c54d3a327cd3ee89cd5c49ad
-    maxDimension = max(trainMax, testMax)
+    testHeaders =   getHeaders(args.test)
+
     print 'processing training data'
 
     i = 0
     classifier = simplePerceptron.perceptron(.5, 5)
-<<<<<<< HEAD
     winnow = Winnow(testHeaders, .05)
     
-=======
-    winnow = Winnow(maxDimension, testHeaders, .05)
-
->>>>>>> c830757188f5de11c54d3a327cd3ee89cd5c49ad
     for row in processDataGenerate(args.test, False):
         #classifier.trainOnExample(row.getSortedColsArr(), row.expected)
         winnow.train(row)
         
     for row in processDataGenerate(args.test, False):
-<<<<<<< HEAD
         #if i % 1000 == 0:
             #print classifier.getPredictionFromVec(row.getSortedColsArr())
         #i += 1
@@ -62,9 +51,3 @@ if __name__ == "__main__":
         print 'real: '  + str(row.expected)
         print '\n'
         print raw_input('press enter')
-=======
-        if i % 1000 == 0:
-            print classifier.getPredictionFromVec(row.getSortedColsArr())
-        i += 1
-
->>>>>>> c830757188f5de11c54d3a327cd3ee89cd5c49ad
